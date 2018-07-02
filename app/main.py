e93bd824aff80f07cb1f5254cc1ff1600f18ee69
Vadym Hutei <hutei@live.com>
2018-07-03 00:37:09 +0300

    [APP] login

--- app/main.py
+++ app/main.py
@@ -1,14 +1,63 @@
-from flask import Flask, render_template
+import datetime
+from random import choice
+from flask import Flask, render_template, request, redirect, url_for, make_response
 
+
+
+# Variables
+ACP_PASSWORD = '123'
+tokens = []
 app = Flask(__name__)
 
+
+
+# Functions
+def createRandomString(n=16):
+    abc = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    return ''.join([choice(abc) for _ in range(n)])
+
+def isAuth():
+    cookie = request.cookies.get('cat')
+    return cookie is not None and cookie in tokens
+
+def auth():
+    password = request.form['password']
+    return password == ACP_PASSWORD
+
+def createToken():
+    token = createRandomString()
+    tokens.append(token)
+    return token
+
+def login():
+    token = createToken()
+    expire_date = datetime.datetime.now()
+    expire_date = expire_date + datetime.timedelta(days=30)
+    response = make_response(redirect(url_for('acp')))
+    response.set_cookie('cat', token, expires=expire_date)
+    return response
+
+
+# App
 @app.route('/', methods=['GET'])
 def main():
     return render_template('main.html')
 
 @app.route('/acp/', methods=['GET'])
 def acp():
+    if not isAuth():
+        return redirect(url_for('acp_login'))
     return render_template('acp/main.html')
+
+@app.route('/acp/login', methods=['GET'])
+def acp_login():
+    return render_template('acp/login.html')
+
+@app.route('/acp/auth', methods=['POST'])
+def acp_auth():
+    if auth():
+        return login()
+    return redirect(url_for('acp_login'))
 
 @app.route('/acp/stores/', methods=['GET'])
 def acp_stores():
